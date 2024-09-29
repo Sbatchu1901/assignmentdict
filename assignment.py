1cf216a64427a8862a28a6b1a8e49db561f084ae
sbatchu1901 <sbatchu@murraystate.edu>
2024-09-28 19:16:57 -0500

    STOCK NUMBERS HAVE BEEN ASSIGNED

--- assignment.py
+++ assignment.py
@@ -24,3 +24,27 @@ appetizer_items = ['A1', 'A2',  'A3', 'A4', 'A5']
 salad_items = ['S1', 'S2', 'S3']
 entree_items = ['E1', 'E2',  'E3', 'E4']
 dessert_items =['T1', 'T2',  'T3', 'T4']
+def get_item_information(item_code):
+  """ this  function that will return the item name and price for a given item code.
+    For example, find_menu_item('D2') should return Lemonade, and integer 3 as the result """
+  for item in menu_items:
+    item_number, item_name, item_price = item.split(' ')
+    if item_number == item_code:
+      return item_name.encode("ascii", "ignore").decode(), (item_price)
+import random
+menu_list = []
+for item in menu_items:
+    item_split = item.split()
+    item_code = item_split[0]
+    item_price = float(item_split[-1])
+    item_name = " ".join(item_split[1:-1]).replace('\u200b', '')
+    menu_item = {
+        "code": item_code,
+        "name": item_name,
+        "price": item_price
+    }
+    if item_code not in drink_items:
+        item_stock = random.randint(25, 50)
+        menu_item["stock"] = item_stock
+    menu_list.append(menu_item)
+print(menu_list)
